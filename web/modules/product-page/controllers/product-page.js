72f33c4a1b4f5948c823b1425864933138caf8ba
Kirill-Murashkin <murashkin.kirill.konst@gmail.com>
2017-05-11 21:57:55 +0200

    TB-2 Product description has been added

--- web/modules/product-page/controllers/product-page.js
+++ web/modules/product-page/controllers/product-page.js
@@ -12,16 +12,32 @@
 
 		'$stateParams',
 
+		'$sce',
+
 		'productsProvider',
 
 		'translitFactory',
 
 		'STATE_NAMES',
 
-		function($scope, $log, $state, $stateParams, productsProvider, translitFactory, STATE_NAMES) {
+		function($scope, $log, $state, $stateParams, $sce, productsProvider, translitFactory, STATE_NAMES) {
             $scope.product = {};
 
             $scope.productCount = 1;
+
+            $scope.descTypes = {
+            	artictic: {
+            		name: 'artictic',
+
+					status: true
+				},
+
+				technical: {
+            		name: 'technical',
+
+					status: false
+				}
+			};
 
             function getFirstCategory(product) {
 				return product.category[_.keys(product.category)[0]][0];
@@ -40,6 +56,79 @@
                             $scope.productCount--;
 						}
 					}
+                },
+
+				changeDescription: function (descType) {
+                	var type;
+
+					for ( type in $scope.descTypes ) {
+						if ( $scope.descTypes.hasOwnProperty(type) ) {
+                            $scope.descTypes[type].status = false;
+						}
+					}
+
+                    $scope.descTypes[descType].status = true;
+                }
+			};
+
+            var productHandlers = {
+            	createArtDescription: function (description) {
+                    $scope.artDescription = $sce.trustAsHtml(description);
+                },
+
+				createTechDescription: function (techDetails) {
+                    var uselessProductAttrNames = ['code', 'article', 'unit'];
+
+                    $scope.product.techDescription = _.toArray(techDetails).filter(function (element, index, arr) {
+                        return uselessProductAttrNames.indexOf(element[1].name) === -1;
+                    });
+                },
+
+				createPrices: function (discounts, originalPrice) {
+            		if ( !_.isEmpty(discounts) ) {
+                        $scope.product.bestPrice = Math.floor(_.minBy(discounts, function (discount) {
+                            return +discount.price;
+                        }).price);
+					}
+
+					$scope.product.oldPrice = Math.floor(originalPrice);
+                },
+
+				createBreadcrumbs: function () {
+                    $scope.breadcrumbs = [
+                        {
+                            name: 'Главная',
+                            state: {
+                                name: STATE_NAMES.HOME,
+                                params: {}
+                            }
+                        },
+
+                        {
+                            name: 'Каталог',
+                            state: {
+                                name: STATE_NAMES.CATEGORIES,
+                                params: {}
+                            }
+                        },
+
+                        {
+                            name: $scope.product.category[_.keys($scope.product.category)[0]][0].name,
+                            state: {
+                                name: STATE_NAMES.CATEGORY,
+
+                                params: {categoryId: getFirstCategory($scope.product).category_id, categoryName: translitFactory.rusTolat(getFirstCategory($scope.product).name)}
+                            }
+                        },
+
+                        {
+                            name: $scope.product.product_description[1].name,
+                            state: {
+                                name: STATE_NAMES.PRODUCT,
+                                params: {}
+                            }
+                        }
+                    ];
                 }
 			};
 
@@ -47,49 +136,20 @@
             	function (response) {
 					$scope.product = response.data.data;
 
-					console.log('$scope.product: ', $scope.product);
+                    console.log('$scope.product: ', $scope.product);
 
-                    $scope.breadcrumbs = [
-                        {
-                            name: 'Главная',
-                            state: {
-                            	name: STATE_NAMES.HOME,
-								params: {}
-							}
-                        },
+                    productHandlers.createArtDescription($scope.product.product_description[1].description);
 
-                        {
-                            name: 'Каталог',
-                            state: {
-                            	name: STATE_NAMES.CATEGORIES,
-								params: {}
-							}
-                        },
+                    productHandlers.createTechDescription($scope.product.product_attributes.attributes);
 
-                        {
-                            name: $scope.product.category[_.keys($scope.product.category)[0]][0].name,
-                            state: {
-                            	name: STATE_NAMES.CATEGORY,
+                    productHandlers.createPrices($scope.product.discounts, $scope.product.price);
 
-								params: {categoryId: getFirstCategory($scope.product).category_id, categoryName: translitFactory.rusTolat(getFirstCategory($scope.product).name)}
-							}
-                        },
-
-                        {
-                            name: $scope.product.product_description[1].name,
-                            state: {
-                            	name: STATE_NAMES.PRODUCT,
-								params: {}
-							}
-                        }
-                    ];
+                    productHandlers.createBreadcrumbs();
                 },
 
 				function (error) {
                     $log.error(error);
                 }
 			);
-
-
 		}]);
 })();
