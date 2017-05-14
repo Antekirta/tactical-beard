e361db1d9c03a6476c558035edd8d3f440f30497
Kirill-Murashkin <murashkin.kirill.konst@gmail.com>
2017-05-14 14:59:33 +0200

    TB-2 Work in progress

--- web/modules/utils/directives/image-popup/image-popup.js
+++ web/modules/utils/directives/image-popup/image-popup.js
@@ -2,27 +2,94 @@
     'use strict';
     
     angular.module('utils')
-        .directive('imagePopup', function () {
+        .directive('imagePopup', ['EVENTS', function (EVENTS) {
             return {
                 restrict: 'E',
+
+                scope: true,
 
                 templateUrl: 'modules/utils/directives/image-popup/image-popup.html',
 
                 link: function (scope, element, attrs) {
+                    // storage for images sources
+                    var sources = [],
+
+                        images,
+
+                        background,
+
+                        image;
+
                     $(document).ready(function () {
-                        var container = $( element.parent() );
-
-                        var images = container.find('img');
-
-                        console.log('element: ', element);
-                        console.log('container: ', container);
-                        console.log('images: ', images);
-
                         scope.imagePopup = {};
 
-                        scope.imagePopup.image = 'http://tacticalbeard39.com/image/cache/import_files/9a4aa975-80b1-407b-a322-bfdd7b342e080-100x100.JPG';
+                        var helpers = {
+                            fillImagesSources: function (sources, images) {
+                                if ( _.isEmpty(sources) ) {
+                                    _.toArray(images).forEach(function (item) {
+                                        sources.push(item.src);
+                                    });
+
+                                    sources = _.compact(_.uniq(sources));
+                                }
+                            }
+                        };
+
+                        // basic settings
+                        var container = element.parent();
+
+                        container.on(EVENTS.ELEMENT.CLICK, function (event) {
+                            var target = event.target;
+
+                            // we declare images only here, insie of click callback, because we have to wait for the images to load
+                            var images = container.find('img');
+
+                            var background = $('.image-popup__dark-background');
+
+                            var image = $('.image-popup__image');
+
+                            helpers.fillImagesSources(sources, images);
+
+
+                            console.log('sources: ', sources);
+
+
+
+                            if ( target.nodeName === 'IMG' ) {
+                                scope.imagePopup.image = target.src;
+
+                                scope.$digest();
+
+                                background.addClass('product-images__dark-background--visible');
+                            }
+
+                            background.on(EVENTS.ELEMENT.CLICK, function () {
+                                $(this).removeClass('product-images__dark-background--visible');
+                            });
+
+                            image.on(EVENTS.ELEMENT.CLICK, function () {
+                                console.log('scope.imagePopup.image before: ', scope.imagePopup.image);
+                                var currentIndex = _.findIndex(sources, function (item) {
+                                    return item === scope.imagePopup.image;
+                                });
+
+                                if ( currentIndex >= sources.length ) {
+                                    currentIndex = 0;
+                                } else {
+                                    currentIndex++;
+                                }
+
+                                console.log('currentIndex: ', currentIndex);
+
+                                scope.imagePopup.image = sources[currentIndex];
+
+                                console.log('scope.imagePopup.image after: ', scope.imagePopup.image);
+
+                                scope.$digest();
+                            });
+                        });
                     });
                 }
             };
-        });
+        }]);
 })();
