92a894bfe4d412ab0f71bdb5c1b31d17639b92c5
Kirill-Murashkin <murashkin.kirill.konst@gmail.com>
2017-09-23 17:25:46 +0200

    beard/1 Scaffold order form

--- web/modules/make-order-page/controllers/make-order-page.js
+++ web/modules/make-order-page/controllers/make-order-page.js
@@ -19,25 +19,47 @@
         'COUNTRIES',
 
         function ($scope, $log, session, countriesFactory, checkoutProvider, basketProvider, regionsProvider, COUNTRIES) {
+            const $orderCtrl = this;
+
+            $orderCtrl.order = {
+                firstname: '',
+                lastname: '',
+                email: '',
+                telephone: '',
+                company: '',
+                city: '',
+                address_1: '',
+                country_id: '',
+                postcode: '',
+                zone_id: '',
+                shipping_method: '',
+                payment_method: '',
+                agree: '',
+                comment: ''
+            };
+
             const params = {
                 currentSession: ''
             };
 
-            $scope.shippingMethods = [];
-            $scope.paymentMethods = [];
-            $scope.orderConfirmed = false;
-
-            $scope.setShippingMethod = setShippingMethod;
-            $scope.setPaymentMethod = setPaymentMethod;
-
-            fillRegionsList();
-
             setCurrentSession();
 
-            $scope.$watch('customerForm.customer.name', userInfoIsFilled);
-            $scope.$watch('customerForm.customer.email', userInfoIsFilled);
-            $scope.$watch('customerForm.customer.tel', userInfoIsFilled);
-            $scope.$watch('customerForm.customer.address', createAddressWatcher);
+            /**
+             * firstname
+             * lastname
+             * email
+             * telephone
+             * company // ?
+             * city
+             * address_1
+             * country_id
+             * postcode
+             * zone_id
+             * shipping_method
+             * payment_method
+             * agree
+             * comment // ?
+             */
 
             function setCurrentSession() {
                 session.getCurrentSession().then(
@@ -45,153 +67,6 @@
                         params.currentSession = session;
                     }
                 );
-            }
-
-            function fillRegionsList() {
-                countriesFactory.getCountryById(COUNTRIES.RUSSIA.ID)
-                    .then(
-                        function (countries) {
-                            $scope.regions = countries;
-                        }
-                    );
-            }
-
-            function fillShippingTypesList(shippingMethods) {
-                for (let method in shippingMethods) {
-                    if ( shippingMethods.hasOwnProperty(method) ) {
-                        $scope.shippingMethods.push({
-                            name: shippingMethods[method].title,
-
-                            value: shippingMethods[method].quote[method] && shippingMethods[method].quote[method].code
-                        });
-                    }
-                }
-            }
-
-            function fillPaymentMethodsList(paymentMethods) {
-                console.log('fillPaymentMethodsList paymentMethods: ', paymentMethods);
-
-                $scope.paymentMethods = [];
-
-                for (let method in paymentMethods) {
-                    if ( paymentMethods.hasOwnProperty(method) ) {
-                        $scope.paymentMethods.push({
-                            name: paymentMethods[method].title,
-
-                            value: paymentMethods[method].code
-                        });
-                    }
-                }
-            }
-
-            function createAddressWatcher() {
-                if ( $scope.customerForm.customer ) {
-                    $scope.$watch('customerForm.customer.address.region', userInfoIsFilled);
-                    $scope.$watch('customerForm.customer.address.city', userInfoIsFilled);
-                    $scope.$watch('customerForm.customer.address.street', userInfoIsFilled);
-                }
-            }
-
-            function userInfoIsFilled() {
-                let customerIsFullfilled =
-                    $scope.customerForm.customer &&
-                    $scope.customerForm.customer.name &&
-                    $scope.customerForm.customer.email &&
-                    $scope.customerForm.customer.tel &&
-                    $scope.customerForm.customer.address &&
-                    $scope.customerForm.customer.address.region;
-
-                if ( customerIsFullfilled ) {
-                    createGuestUser($scope.customerForm.customer);
-                }
-            }
-
-            function createGuestUser(customer) {
-                const zoneId = _.find($scope.regions, function (region) {
-                    return region.name === customer.address.region;
-                })['zone_id'];
-
-                const customerInfo = {
-                    'firstname': customer.name,
-                    'lastname': customer.name,
-                    'email': customer.email,
-                    'telephone': customer.tel,
-                    'fax': customer.tel,
-                    'company': '',
-                    'city': customer.address.city || 'Not specified',
-                    'address_1': customer.address.street || 'Not specified',
-                    'address_2': '',
-                    'country_id': COUNTRIES.RUSSIA.ID,
-                    'postcode': '',
-                    'zone_id': zoneId
-                };
-
-                if ( $scope.customerForm.$valid ) {
-                    checkoutProvider.createGuest(params.currentSession, customerInfo)
-                        .then(
-                            function () {
-                                return checkoutProvider.setGuestShipping(params.currentSession, customerInfo);
-                            }
-                        )
-                        .then(
-                            function () {
-                                return checkoutProvider.getShippingMethods(params.currentSession)
-                                    .then(
-                                        function (response) {
-                                            if ( response.success ) {
-                                                if ( _.isEmpty($scope.shippingMethods) ) {
-                                                    fillShippingTypesList(response.data['shipping_methods']);
-                                                }
-                                            }
-                                        }
-                                    );
-                            }
-                        );
-                }
-            }
-
-            function setShippingMethod(method) {
-                checkoutProvider.setShippingMethods(params.currentSession, method.value, method.name)
-                    .then(
-                        function (response) {
-                            if ( response.data.success ) {
-                                $log.log('Shipping method has been successfully set!');
-                            }
-                        }
-                    )
-                    .then(
-                        function () {
-                            checkoutProvider.getPaymentMethods(params.currentSession)
-                                .then(
-                                    function (paymentMethods) {
-                                        fillPaymentMethodsList(paymentMethods.data['payment_methods']);
-                                    }
-                                );
-                        }
-                    );
-            }
-
-            function setPaymentMethod(method) {
-                checkoutProvider.setPaymentMethod(params.currentSession, method.value, method.name, true)
-                    .then(
-                        function (response) {
-                            if ( response.data.success ) {
-                                $log.log('Payment method has been successfully set!');
-                            }
-                        }
-                    )
-                    .then(
-                        function () {
-                            checkoutProvider.confirm(params.currentSession)
-                                .then(
-                                    function (response) {
-                                        $scope.orderConfirmed = response.success;
-
-                                        $scope.totalOrderInfo = response.data.payment;
-                                    }
-                                );
-                        }
-                    );
             }
         }]);
 })();
